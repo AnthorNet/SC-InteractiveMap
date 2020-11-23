df0e06920f6a5b8240f0f5e106b88f9ffa02fd2e
AnthorNet <anthor.net@gmail.com>
2020-11-23 15:12:24 +0100

    Implement Sentry release sourcemap

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,59 @@
 'use strict';
 
 const path                  = require('path');
+
+const SentryWebpackPlugin   = require('@sentry/webpack-plugin');
 const TerserPlugin          = require("terser-webpack-plugin");
 const JsonMinimizerPlugin   = require("json-minimizer-webpack-plugin");
 const MergeJsonPlugin       = require("merge-json-webpack-plugin");
 const CopyPlugin            = require("copy-webpack-plugin");
 
-module.exports = {
-    mode            : 'production',
-    devtool         : 'source-map',
-    performance     : { hints: false },
-    context         : path.resolve(__dirname, 'src'),
-    entry           : './SCIM.js',
+module.exports = env => {
 
-    output          : {
-        path            : path.resolve(__dirname, 'build'),
-        filename        : './SCIM.js'
-    },
+    return {
+        mode            : 'production',
+        devtool         : 'source-map',
+        performance     : { hints: false },
+        context         : path.resolve(__dirname, 'src'),
+        entry           : './SCIM.js',
 
-    optimization    : {
-        minimize        : true,
-        minimizer       : [
-            new TerserPlugin({test: /\.js(\?.*)?$/i}),
-            new JsonMinimizerPlugin({test: /\.json(\?.*)?$/i})
+        output          : {
+            path            : path.resolve(__dirname, 'build'),
+            filename        : './SCIM.js'
+        },
+
+        optimization    : {
+            minimize        : true,
+            minimizer       : [
+                new TerserPlugin({test: /\.js(\?.*)?$/i}),
+                new JsonMinimizerPlugin({test: /\.json(\?.*)?$/i})
+            ]
+        },
+
+        plugins: [
+            /* Merge all detailed models into a single JSON file... */
+            new MergeJsonPlugin({
+                group: [
+                    { files: './Models/*.json', to: './detailedModels.json' }
+                ]
+            }),
+            /* Copy pipe fonts */
+            new CopyPlugin({
+                patterns: [
+                    { from: "./PipeFonts/*.json" }
+                ]
+            }),
+            new SentryWebpackPlugin({
+                // sentry-cli configuration
+                url: env.SENTRY_URL,
+                authToken: env.SENTRY_AUTH_TOKEN,
+                org: "sentry",
+                project: "scim",
+
+                // webpack specific configuration
+                include: path.resolve(__dirname, 'src'),
+                ignore: ["node_modules", "webpack.config.js"]
+            })
         ]
-    },
-
-    plugins: [
-        /* Merge all detailed models into a single JSON file... */
-        new MergeJsonPlugin({
-            group: [
-                { files: './Models/*.json', to: './detailedModels.json' }
-            ]
-        }),
-        new CopyPlugin({
-            patterns: [
-                { from: "./PipeFonts/*.json" }
-            ]
-        })
-    ]
+    };
 };
-
-
-
