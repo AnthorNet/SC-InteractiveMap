770ffaa3425ecc1f61eda099a99857990289e979
AnthorNet <anthor.net@gmail.com>
2020-11-25 15:23:33 +0100

    Revert minification while investigating source-map with Sentry

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,57 @@
 'use strict';
 
-const path                  = require('path');
+const webpack                       = require('webpack');
+const path                          = require('path');
 
-const SentryWebpackPlugin   = require('@sentry/webpack-plugin');
-const TerserPlugin          = require("terser-webpack-plugin");
-const JsonMinimizerPlugin   = require("json-minimizer-webpack-plugin");
-const MergeJsonPlugin       = require("merge-json-webpack-plugin");
-const CopyPlugin            = require("copy-webpack-plugin");
+const SentryWebpackPlugin           = require('@sentry/webpack-plugin');
+const TerserPlugin                  = require("terser-webpack-plugin");
+const JsonMinimizerPlugin           = require("json-minimizer-webpack-plugin");
+const MergeJsonPlugin               = require("merge-json-webpack-plugin");
+const CopyPlugin                    = require("copy-webpack-plugin");
 
 module.exports = env => {
-
     return {
         mode            : 'production',
         devtool         : 'source-map',
         performance     : { hints: false },
         context         : path.resolve(__dirname, 'src'),
-        entry           : './SCIM.js',
-
-        output          : {
-            path            : path.resolve(__dirname, 'build'),
-            filename        : './SCIM.js'
+        entry           : {
+            //SaveParser      : './SaveParser.js',
+            //BaseLayout      : './BaseLayout.js',
+            //Map             : './Map.js',
+            SCIM            : './SCIM.js'
         },
 
+        output          : {
+            //library         : '[name]',
+
+            path            : path.resolve(__dirname, 'build'),
+            filename        : './[name].js'
+        },
         optimization    : {
             minimize        : true,
             minimizer       : [
-                new TerserPlugin({test: /\.js(\?.*)?$/i}),
+                //new TerserPlugin({test: /\.js(\?.*)?$/i}),
                 new JsonMinimizerPlugin({test: /\.json(\?.*)?$/i})
             ]
         },
 
         plugins: [
-            /* Merge all detailed models into a single JSON file... */
+            // Merge all detailed models into a single JSON file...
             new MergeJsonPlugin({
                 group: [
                     { files: './Models/*.json', to: './detailedModels.json' }
                 ]
             }),
-            /* Copy pipe fonts */
+
+            // Copy pipe fonts
             new CopyPlugin({
                 patterns: [
                     { from: "./PipeFonts/*.json" }
                 ]
             }),
+
+            // Send new release to Sentry
             new SentryWebpackPlugin({
                 // sentry-cli configuration
                 url: env.SENTRY_URL,
@@ -51,8 +60,10 @@ module.exports = env => {
                 project: "scim",
 
                 // webpack specific configuration
-                include: path.resolve(__dirname, 'src'),
-                ignore: ["node_modules", "webpack.config.js"]
+                validate: true,
+                include: path.resolve(__dirname, 'build'),
+                //include: path.resolve(__dirname, 'src'),
+                ignore: ['node_modules', 'webpack.config.js']
             })
         ]
     };
