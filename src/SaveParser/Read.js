804be8cba8da126d558ab6c338a5cd34e7abe923
AnthorNet <anthor.net@gmail.com>
2020-12-01 10:26:07 +0100

    Add "FFSlimeProcessingTask" to StructProperty

--- src/SaveParser/Read.js
+++ src/SaveParser/Read.js
@@ -877,6 +877,7 @@ export default class SaveParser_Read
                     case 'FFCompostingTask': // MOD: ???
                     case 'FFSeedExtrationTask': // MOD: ???
                     case 'FFSlugBreedTask': // MOD: ???
+                    case 'FFSlimeProcessingTask': // MOD: ???
                     case 'SInventory': // MOD: ???
                         currentProperty.value.values = [];
                         while(true)
