59737aa300929ce1fbfa7dbe2d03599e0b753c8d
AnthorNet <anthor.net@gmail.com>
2024-03-26 11:31:49 +0100

    Fix typo (Close #323)

--- src/Modal/Map/Options.js
+++ src/Modal/Map/Options.js
@@ -172,13 +172,13 @@ export default class Modal_Map_Options
         html.push('<div class="form-group"><div class="input-group input-group-sm"><div class="input-group-prepend"><label class="input-group-text" for="inputMapStructuresModelsQuality">Structures Models Quality</label></div><select class="custom-select text-white" id="inputMapStructuresModelsQuality" style="background: none;">');
         html.push('<option value="low" class="text-secondary" ' + ((this.baseLayout.mapStructuresModelsQuality === 'low') ? 'selected' : '') + '>Square (Low)</option>');
         html.push('<option value="medium" class="text-secondary" ' + ((this.baseLayout.mapStructuresModelsQuality === 'medium') ? 'selected' : '') + '>Outline (Medium)</option>');
-        html.push('<option value="high" class="text-secondary" ' + ((this.baseLayout.mapStructuresModelsQuality === 'high') ? 'selected' : '') + '>Ouline + Details (High)</option>');
+        html.push('<option value="high" class="text-secondary" ' + ((this.baseLayout.mapStructuresModelsQuality === 'high') ? 'selected' : '') + '>Outline + Details (High)</option>');
         html.push('</select></div></div>');
         html.push('</div><div class="col-6">');
         html.push('<div class="form-group"><div class="input-group input-group-sm"><div class="input-group-prepend"><label class="input-group-text" for="inputMapModelsQuality">BuildingsModels Quality</label></div><select class="custom-select text-white" id="inputMapModelsQuality" style="background: none;">');
         html.push('<option value="low" class="text-secondary" ' + ((this.baseLayout.mapModelsQuality === 'low') ? 'selected' : '') + '>Square (Low)</option>');
         html.push('<option value="medium" class="text-secondary" ' + ((this.baseLayout.mapModelsQuality === 'medium') ? 'selected' : '') + '>Outline (Medium)</option>');
-        html.push('<option value="high" class="text-secondary" ' + ((this.baseLayout.mapModelsQuality === 'high') ? 'selected' : '') + '>Ouline + Details (High)</option>');
+        html.push('<option value="high" class="text-secondary" ' + ((this.baseLayout.mapModelsQuality === 'high') ? 'selected' : '') + '>Outline + Details (High)</option>');
         html.push('</select></div></div>');
         html.push('</div></div>');
 
