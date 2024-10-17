d59f28327a5634f04eaf720c7fc57d16c922cea3
AnthorNet <anthor.net@gmail.com>
2024-10-17 12:26:01 +0200

    Fix normalization of point rotation

--- src/BaseLayout/Math.js
+++ src/BaseLayout/Math.js
@@ -147,11 +147,11 @@ export default class BaseLayout_Math
         {
             eulerAngle.roll = 120;
         }
-        if(Math.round(BaseLayout_Math.normalizeEulerAxis(eulerAngle.roll)) > -60 && Math.round(BaseLayout_Math.normalizeEulerAxis(eulerAngle.roll)) < -90)
+        if(Math.round(BaseLayout_Math.normalizeEulerAxis(eulerAngle.roll)) < -60 && Math.round(BaseLayout_Math.normalizeEulerAxis(eulerAngle.roll)) > -90)
         {
             eulerAngle.roll = -60;
         }
-        if(Math.round(BaseLayout_Math.normalizeEulerAxis(eulerAngle.roll)) >= -90 && Math.round(BaseLayout_Math.normalizeEulerAxis(eulerAngle.roll)) < -120)
+        if(Math.round(BaseLayout_Math.normalizeEulerAxis(eulerAngle.roll)) <= -90 && Math.round(BaseLayout_Math.normalizeEulerAxis(eulerAngle.roll)) > -120)
         {
             eulerAngle.roll = -120;
         }
@@ -243,6 +243,7 @@ export default class BaseLayout_Math
 
         return sign * p;
     }
+
     static scalarSin(value)
     {
         // Map Value to y in [-pi,pi], x = 2*pi*quotient + remainder.
@@ -321,6 +322,39 @@ export default class BaseLayout_Math
 
         return y + 1;
     }
+
+    /*
+    static blendColor(from, to, p = 0.5) {
+        from = from.trim();
+        to = to.trim();
+        const b = p < 0;
+        p = b ? p * -1 : p;
+        const f = toRGBA(from);
+        const t = toRGBA(to);
+        if (to[0] === 'r') {
+          return 'rgb' + (to[3] === 'a' ? 'a(' : '(') +
+          r((t[0] - f[0]) * p + f[0]) + ',' +
+          r((t[1] - f[1]) * p + f[1]) + ',' +
+          r((t[2] - f[2]) * p + f[2]) + (
+          f[3] < 0 && t[3] < 0 ? '' : ',' + (
+          f[3] > -1 && t[3] > -1 ?
+          r(((t[3] - f[3]) * p + f[3]) * 10000) / 10000 :
+          t[3] < 0 ? f[3] : t[3])) +
+
+          ')';
+        }
+
+        return '#' + (0x100000000 + (
+        f[3] > -1 && t[3] > -1 ?
+        r(((t[3] - f[3]) * p + f[3]) * 255) :
+        t[3] > -1 ? r(t[3] * 255) : f[3] > -1 ? r(f[3] * 255) : 255) *
+        0x1000000 +
+        r((t[0] - f[0]) * p + f[0]) * 0x10000 +
+        r((t[1] - f[1]) * p + f[1]) * 0x100 +
+        r((t[2] - f[2]) * p + f[2])).
+        toString(16).slice(f[3] > -1 || t[3] > -1 ? 1 : 3);
+    }
+    */
 
     static extractSplineData(baseLayout, currentObject)
     {
