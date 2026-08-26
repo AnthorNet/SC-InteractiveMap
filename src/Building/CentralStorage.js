0270752172ed378ed83d2454ca3442bb2e5cdb7c
AnthorNet <anthor.net@gmail.com>
2026-08-26 11:55:34 +0200

    Update Dimensional Depot tooltip (#489)

--- src/Building/CentralStorage.js
+++ src/Building/CentralStorage.js
@@ -39,22 +39,36 @@ export default class Building_CentralStorage
         let content             = [];
         let storageContent      = Building_CentralStorage.getCurrentStorage(baseLayout, currentObject);
 
-        content.push('<strong class="text-danger">Work in Progress</strong><br />');
-        content.push('<br />');
-
         if(storageContent !== null)
         {
-            let currentItem         = baseLayout.getItemDataFromClassName(storageContent.currentItem);
-                content.push('<table class="mx-auto"><tr><td><div style="animation: interference 4s infinite;">' + baseLayout.getInventoryImage(currentItem, 96) + '</div></td></tr></table>');
-                content.push('<br />');
+            content.push('<div style="position: absolute;margin-top: 170px;margin-left: 66px; width: 188px;height: 250px;">');
+
+                let currentItem         = baseLayout.getItemDataFromClassName(storageContent.currentItem);
+                    content.push('<table class="mx-auto"><tr><td><div style="animation: interference 4s infinite;">' + baseLayout.getInventoryImage({
+                        category        : currentItem.category,
+                        name            : currentItem.name,
+                        image           : currentItem.image,
+                        qty             : storageContent.currentAmount
+                    }, 96) + '</div></td></tr></table>');
+
+                // Progression
+                let uploadRate      = baseLayout.centralStorageSubSystem.getUploadRate();
+                let mUploadTimer    = baseLayout.getObjectProperty(currentObject, 'mUploadTimer');
+                let uploadProgress  = mUploadTimer / (60 / uploadRate) * 100;
+                    content.push('<div style="padding: 0 30px;"><div class="progress rounded-sm mx-2 mt-2" style="height: 10px;"><div class="progress-bar bg-warning" style="width: ' + uploadProgress + '%"></div></div></div>');
+
+            content.push('</div>');
+
+            // Working light
+            content.push('<div style="position: absolute;margin-top: 0px;margin-left: 60px; width: 200px;height: 90px;"><img src="' + baseLayout.staticUrl + '/js/InteractiveMap/img/TXUI_CentralStorage_LightWeak.png?v=' + baseLayout.scriptVersion + '" /></div>');
+            content.push('<div style="position: absolute;margin-top: 0px;margin-left: 60px; width: 200px;height: 90px;" class="blink"><img src="' + baseLayout.staticUrl + '/js/InteractiveMap/img/TXUI_CentralStorage_LightStrong.png?v=' + baseLayout.scriptVersion + '" /></div>');
         }
 
-        content.push('Depot Upload Rate: ' + baseLayout.centralStorageSubSystem.getUploadRate() + ' per min');
+        // Upload rate
+        content.push('<div style="position: absolute;margin-top: 370px;margin-left: 66px; width: 188px;height: 48px;text-align: center;color: #FFFFFF;">');
+        content.push('Depot Upload Rate:<br />' + baseLayout.centralStorageSubSystem.getUploadRate() + ' per min');
+        content.push('</div>');
 
-        return '<div class="d-flex" style="border: 25px solid #7f7f7f;border-image: url(' + baseLayout.staticUrl + '/js/InteractiveMap/img/genericTooltipBackground.png?v=' + baseLayout.scriptVersion + ') 25 repeat;background: #7f7f7f;margin: -7px;' + BaseLayout_Tooltip.defaultTextStyle + '">\
-                    <div class="justify-content-center align-self-center w-100 text-center" style="margin: -10px 0;">\
-                        ' + content.join('') + '\
-                    </div>\
-                </div>';
+        return '<div style="width: 320px;height: 470px;background: url(' + baseLayout.staticUrl + '/js/InteractiveMap/img/TXUI_CentralStorage_BG.png?v=' + baseLayout.scriptVersion + ') no-repeat;margin: -7px;">' + content.join('') + '</div>';
     }
 }
